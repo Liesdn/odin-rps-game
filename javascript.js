b5f13f4bb02637565cf94380cb052e9379257fa9
lies <dc.lipari@gmail.com>
2023-03-22 13:39:16 -0300

    Finish rps game with simple UI

--- javascript.js
+++ javascript.js
@@ -1,70 +1,53 @@
 const options = ['Rock', 'Paper', 'Scissors'];
 let playerScore = 0;
 let compScore = 0;
-let gameNo = 1;
+const buttons = document.querySelectorAll('input');
 
+function disableButtons() {
+    buttons.forEach(element => {
+        element.disabled = true
+    })
+}
 
 function getCompChoice(){
     let compChoice = Math.floor(Math.random() * options.length);
     return options[compChoice];
 }
 
-function getPlayerChoice() {
-    let validChoice = false;
-    while (validChoice == false) {
-        let choice = prompt('Enter rock, paper or scissors');
-        if (choice == null) {
-            continue;
-        }
-        let choiceCap = choice.charAt(0).toUpperCase() + choice.slice(1).toLowerCase() 
-        if (options.includes(choiceCap)) {
-            validChoice = true;
-            return choiceCap;
-        }
-    }
-
-}
-
-function playRound(playerChoice, compChoice) {
-    console.log(`You chose '${playerChoice}'`);
-    console.log(`Computer chose '${compChoice}'`);
+function playRound(playerChoice) {
+    let compChoice = getCompChoice();
+    let result = '';
     if (playerChoice == compChoice) {
-        return 'It\'s a tie';
-    }
-    else if (
+        result = (`It's a tie. You and the Computer both chose ${playerChoice}.` + 
+        `<br><br>You: ${playerScore}, Computer: ${compScore}`);
+        
+    } else if (
         (playerChoice == 'Rock' && compChoice == 'Scissors') ||
         (playerChoice == 'Scissors' && compChoice == 'Paper') ||
         (playerChoice == 'Paper' && compChoice == 'Rock')
     ) {
         playerScore++;
-        return `You win!`;
-    }
-    else {
+        result = (`You win! ${playerChoice} beats ${compChoice}!` +
+        `<br><br> You: ${playerScore}, Computer: ${compScore}`);
+        if (playerScore == 5) {
+            result += `<br><br>You won the game!`;
+            disableButtons();
+        }
+    } else {
         compScore++;
-        return 'You lose!';
+        result = (`You lose! ${compChoice} beats ${playerChoice}!` +
+        `<br><br> You: ${playerScore}, Computer: ${compScore}`);
+        if (compScore == 5) {
+            result += `<br><br>You lost the game...`;
+            disableButtons();
+        }
     }
+    document.getElementById('result').innerHTML = result
+    return
 }
 
-function game() {
-    console.log('Start game')
-    for (let i = 0; i < 5; i++) {
-        let playerChoice = getPlayerChoice();
-        let compChoice = getCompChoice();
-        console.log(`Game N° '${gameNo}'`)
-        console.log(playRound(playerChoice, compChoice));
-        console.log(`You: '${playerScore}'`);
-        console.log(`Computer: '${compScore}'`);
-        gameNo++;
-    }
-    if (playerScore == compScore) {
-        console.log('Tie!');
-    }
-    else if (playerScore > compScore) {
-        console.log('You won the game!');
-    }
-    else {
-        console.log('You lost the game...')
-    }
-}
-
-game();
+buttons.forEach(button =>{
+    button.addEventListener('click', function(){
+        playRound(button.value)
+    })
+})
